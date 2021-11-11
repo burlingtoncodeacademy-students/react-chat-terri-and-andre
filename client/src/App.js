5da9fb3c80d54f89d2d229f560b041682e5f42b1
Terri Isidro <terri.isidro@gmail.com>
2021-11-11 15:52:53 -0500

    starter stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+//----Necessary imports
+import { BrowserRouter, Route, Routes } from 'react-router-dom' //sets up React Router
+import './App.css'
 
-function App() {
+//App function to render page structural elements and BrowserRouter/Routes
+function App () {
   return (
-    <div className="App">
-      <h1>Hello, Chat!</h1>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Routes>
+          <div className='App'>
+            <h1>Super Fun Chat App</h1>
+          </div>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+//necessary imports
+import React from 'react'
+
+export default function Login () {
+  //Return renders the appearance of the page
+  return (
+    <div>
+      <h1>Login to chat:</h1>
+    </div>
+  )
+}
